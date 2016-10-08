270a04bf73a460e14ec8816dca16aabefbb4e831
Eduardo Luiz Divino Rebelo <rebelo@cit>
2016-10-08 16:03:32 -0300

    Version 0.9

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,58 +1,126 @@
+/*
+ 14050-170
+ 13085-485
+ 14020-260
+ 1402-260
+
+ */
+
 $(document).ready(function () {
 
-    $("#cep_button").click(function () {
-        $("#cep_button").attr('disabled', true);
-        $("#cep_loading").css("display", "block");
 
-        var cep = $.trim($("#cep_value").val());
+    function HomeViewModel() {
+        $("#cep_value").mask('00000-000');
 
-        if (cep == "" || cep.length < 8) {
+        var self = this, public_place, neighborhood, city, istate, zip_code, zip_code_temp, full_address;
 
+        self.full_address = ko.observable("");
+        self.url_maps = ko.observable("");
+        self.details_maps = ko.observable("");
+
+        $("#cep_button").click(function () {
+            /* Disabled alerts messages */
+            $("#cep_loading").css("display", "none");
+            $("#cep_error").css("display", "none");
+            $("#cep_warning").css("display", "none");
+
+            /* Enable alerts messages */
+            $("#cep_button").attr('disabled', true);
+            $("#cep_loading").css("display", "block");
+
+            /* Get the value CEP */
+            var cep = $.trim($("#cep_value").val());
+            cep = cep.replace("-", "");
+
+            /* Check if CEP is invalid */
+            if (cep == "" || cep.length < 7) {
+
+                $("#cep_button").attr('disabled', false);
+                $("#cep_loading").css("display", "none");
+                $("#cep_error").css("display", "block");
+                self.full_address("");
+
+                setTimeout(function () {
+                    $("#cep_error").css("display", "none");
+                }, 10000);
+
+            } else {
+                /* Get the location by CEP */
+                $.ajax({
+                    type: "GET",
+                    /*headers: {"Access-Control-Allow-Origin": "*"},*/
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "jsonp",
+                    url: "http://api.postmon.com.br/v1/cep/" + cep,
+                    statusCode: {
+                        200: function (data) {
+                            full_address = "";
+                            if (data.logradouro != null && data.bairro != null) {
+                                public_place = data.logradouro;
+                                neighborhood = data.bairro;
+                                city = data.cidade;
+                                istate = data.estado;
+                                zip_code = data.cep;
+                                full_address = public_place + ", " + neighborhood + ", " + city + " - " + istate + ", " +
+                                    zip_code.substr(0, 5) + "-" + zip_code.substr(5, 8);
+                            } else {
+                                public_place = "";
+                                neighborhood = ""
+                                city = data.cidade;
+                                istate = data.estado;
+                                zip_code = data.cep;
+                                full_address = city + " - " + istate + ", " +
+                                    zip_code.substr(0, 5) + "-" + zip_code.substr(5, 8);
+                            }
+                            self.full_address(full_address);
+                            self.url_maps("http://maps.google.com/?q=" + zip_code);
+                            self.details_maps("Location in Google Maps");
+                            console.log(data);
+                            $("#cep_button").attr('disabled', false);
+                            $("#cep_loading").css("display", "none");
+                        },
+                        201: function () {
+                            console.log("201");
+                            alerts_warning();
+                        },
+                        401: function () {
+                            console.log("TIMEOUT: Token Access Expired");
+                            alerts_warning();
+                        },
+                        403: function () {
+                            console.log("UNAUTHORIZED: User Without Access");
+                            alerts_warning();
+                        },
+                        404: function () {
+                            console.log("ERROR 404: Not Found");
+                            alerts_warning();
+                        },
+                        500: function () {
+                            console.log("ERROR 500: Internal Server Error");
+                            alerts_warning();
+                        }
+                    },
+                    error: function (objRequest) {
+                        console.log("objRequest");
+                        alerts_warning();
+                    }
+                });
+            }
+        });
+
+        /* Function to show warnings meessages */
+        function alerts_warning() {
+            self.full_address("");
             $("#cep_button").attr('disabled', false);
             $("#cep_loading").css("display", "none");
-            $("#cep_error").css("display", "block");
+            $("#cep_warning").css("display", "block");
 
             setTimeout(function () {
-                $("#cep_error").css("display", "none");
+                $("#cep_warning").css("display", "none");
             }, 10000);
-
-        } else {
-
-            $.ajax({
-                type: "GET",
-                //headers: { "Authorization": 'Bearer ' + token },
-                header: ("Access-Control-Allow-Origin: *"),
-                contentType: "application/json; charset=utf-8",
-                dataType: "jsonp",
-                url: "http://api.postmon.com.br/v1/cep/" + cep,
-                statusCode: {
-                    200: function (data) {
-                        //self.projects([]);
-                        //self.projects(data.items);
-                        //$('[data-toggle="tooltip"]').tooltip();
-                        /*for (var i in data) {
-                            for (var j in data[i]) {
-                                console.log(data[i][j].nameProject);
-                            }
-                        }*/
-                        console.log(data)
-                    },
-                    401: function () {
-                        console.log("TIMEOUT: token access expired");
-                    },
-                    403: function () {
-                        console.log("UNAUTHORIZED: user without access");
-                    },
-                    500: self.showErrorMessage
-                }
-            }).error(function () {
-                console.log("UNAUTHORIZED: user without access");
-            });
-
-            $("#cep_button").attr('disabled', false);
-            $("#cep_loading").css("display", "none");
         }
+    }
 
+    ko.applyBindings(new HomeViewModel());
 
-    });
 });
